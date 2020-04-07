09508cf1246e2d097b081b4f6c92365f563bf857
AnimeBytes <staff@animebytes.tv>
2020-04-07 19:21:54 +0200

    sync with 1c9a937d0fe1ba60b87389520abfef77722ccda6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SupportBot.Mixfile do
     [
      app: :support_bot,
      version: "1.3.1",
-     elixir: "~> 1.9",
+     elixir: "~> 1.9.0",
      elixirc_options: [warnings_as_errors: true],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -23,11 +23,11 @@ defmodule SupportBot.Mixfile do
   defp deps do
     [
       {:kaguya, "~> 0.6.6"},
-      {:httpoison, "~> 1.6"},
-      {:timex, "~> 3.0"},
-      {:poison, "~> 4.0"},
-      {:puid, "~> 1.0"},
-      {:distillery, "~> 2.0"}
+      {:httpoison, "~> 1.6.2"},
+      {:timex, "~> 3.6.1"},
+      {:poison, "~> 4.0.1"},
+      {:puid, "~> 1.1.1"},
+      {:distillery, "~> 2.1.1"}
     ]
   end
 end
