784915d4441fbf7323579bd13a64cc22fc6e0ca3
AnimeBytes <staff@animebytes.tv>
2021-10-31 04:35:05 +0100

    Sync with 0853b9e731a96257ed23385b90c31308aff186cd

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 
+## v3.1.0
+### Changed
+- Use specific User-Agent
+
 ## v3.0.0
 ### Changed
 - Bumped minimum supported Node version to v16.13.0
--- src/clients/animebytes.ts
+++ src/clients/animebytes.ts
@@ -5,8 +5,9 @@ import type { ReEnableResponse } from '../types';
 const logger = getLogger('AnimeBytesClient');
 
 export class ABClient {
-  public static REQUEST_TIMEOUT_MS = 30000;
+  public static REQUEST_TIMEOUT_MS = 1000 * 30; // 30 seconds
   public static got = got.extend({
+    headers: { 'User-Agent': 'renge/2.0 (got [ABClient])' },
     followRedirect: false,
     throwHttpErrors: false,
     timeout: ABClient.REQUEST_TIMEOUT_MS,
